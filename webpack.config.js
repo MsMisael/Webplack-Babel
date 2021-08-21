b9a910ec1d740e06944eede6152b06e296bc5cf0
Misael Silva de Souza <msmisael@outlook.com>
2021-08-21 17:04:06 -0300

    Babel
    Adicionei o básico do babel.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     entry: './src/index.js',
     output: { path: __dirname + '/dist/', filename: 'bundle.js' },
-
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+        ]
+    }
 }
