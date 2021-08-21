8798751e6dc57c60ee7bc9a63af84350ca6a42ec
Misael Silva de Souza <msmisael@outlook.com>
2021-08-21 17:07:52 -0300

    plugin html
    Adicionei o plugin html no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: './src/index.js',
     output: { path: __dirname + '/dist/', filename: 'bundle.js' },
+    plugins: [
+        new HtmlWebPackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ],
     module:{
         rules:[
             {
